ffe9ef5071faa7e6d787e6abf998e3e7c10acbbc
Todd Walraven <cod4247@yahoo.com>
2020-04-13 14:42:57 -0700

    update coinChange function
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: K. Wicz <katy@solovewicz.com>

--- src/coinCounter.js
+++ src/coinCounter.js
@@ -1,17 +1,17 @@
-const change = {Quarter: .25, Nickel: .05, Dime: .1, Penny: .01}
+// const change = {Quarter: .25, Nickel: .05, Dime: .1, Penny: .01}
 
-const coinCounter = (counter) => {
-  if (counter === 0)
-  {
-    return [];
-  }
-  else{
-    return coinCounter()
-  }
-}
+// const coinCounter = (counter) => {
+//   if (counter === 0)
+//   {
+//     return [];
+//   }
+//   else{
+//     return coinCounter()
+//   }
+// }
 
 
-const coinChange = cents => {
+export const coinChange = (cents) => {
   let quarter = Math.floor(cents/25);
   cents -= 25*quarter
   let dime = Math.floor(cents/10);
@@ -19,7 +19,8 @@ const coinChange = cents => {
   let nickel = Math.floor(cents/5);
   cents -= 5*nickel
   let penny = cents;
-  console.log('Quarters; ${quarter}, Dimes: ${dime}, Nickels: ${nickel}, Pennies ${penny}')
+  console.log('Quarters: ${quarter}, Dimes: ${dime}, Nickels: ${nickel}, Pennies: ${penny}');
+  return ('Quarters: ${quarter}, Dimes: ${dime}, Nickels: ${nickel}, Pennies: ${penny}');
 };
 
 
@@ -29,37 +30,37 @@ const coinChange = cents => {
 //add chosen coin to change
 //pass remaining money back into function
 
-const coins = {
-  0: {
-    coin = "Quarter",
-    value = .25,
-    count = 0
-  },
-  1: {
-    coin = "Dime",
-    value = .10,
-    count = 0
-  },
-  2: {
-    coin = "Nickel",
-    value = .05,
-    count = 0
-  },
-  3: {
-    coin = "Penny",
-    value = .01,
-    count = 0
-  }
-}
-var index = 0
+// const coins = {
+//   0: {
+//     coin = "Quarter",
+//     value = .25,
+//     count = 0
+//   },
+//   1: {
+//     coin = "Dime",
+//     value = .10,
+//     count = 0
+//   },
+//   2: {
+//     coin = "Nickel",
+//     value = .05,
+//     count = 0
+//   },
+//   3: {
+//     coin = "Penny",
+//     value = .01,
+//     count = 0
+//   }
+// }
+// var index = 0
 
-const coinCounterCaty = (amount, index) => {
-  if (amount === 0) {
-    return 0;
-  } else {
-    console.log(amount)
-    coins.index.count = amount/coins.index.value// first half shoudl be the value of the 0th index, second half should be the key of the 0th index
-    amount -= (coins.index.counter * coins.index.value) //this time, I'm talking about the value of hte 0th index
-    return coinCounter(amount, index + 1)
-  }
-}
+// const coinCounterCaty = (amount, index) => {
+//   if (amount === 0) {
+//     return 0;
+//   } else {
+//     console.log(amount)
+//     coins.index.count = amount/coins.index.value// first half shoudl be the value of the 0th index, second half should be the key of the 0th index
+//     amount -= (coins.index.counter * coins.index.value) //this time, I'm talking about the value of hte 0th index
+//     return coinCounter(amount, index + 1)
+//   }
+// }
