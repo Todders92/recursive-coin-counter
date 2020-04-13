5362ac76a028dd409a23b1826ff8648eab941846
Todd Walraven <cod4247@yahoo.com>
2020-04-13 15:08:23 -0700

    update tests file and recursive coin counter
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: K. Wicz <katy@solovewicz.com>

---
+++ __tests__/recursiveCoinCounter.test.js
@@ -0,0 +1,10 @@
+import { coinCounter } from '../src/recursiveCoinCounter.js';
+
+describe('', () => {
+
+  test('should return number of quarters', () => {
+    var coins = coinCounter();
+    expect(coins).toEqual();
+  });
+
+});
